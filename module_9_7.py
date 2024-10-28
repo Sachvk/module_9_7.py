b547b7cbbac629d5c7c90457965b05a97e2c9a53
Sachvk <serg22011985@gmail.com>
2024-10-28 17:35:03 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,17 @@
+def is_prime(func):
+    def wrapper(*args):
+        result_func = func(*args)
+        if result_func % 2:
+            print(f'Простое')
+        else:
+            print(f'Составное')
+        return result_func
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+   return sum([a, b, c])
+
+result = sum_three(2, 3, 6)
+print(result)
